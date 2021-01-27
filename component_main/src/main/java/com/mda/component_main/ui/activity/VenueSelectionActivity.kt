851564e1d2670b2a1970f4ad767c4ef93e6d9621
3ompact <qhwwjf@gmail.com>
2021-01-27 19:38:41 +0800

    test  VenueSelectionActivity.kt function

--- component_main/src/main/java/com/mda/component_main/ui/activity/VenueSelectionActivity.kt
+++ component_main/src/main/java/com/mda/component_main/ui/activity/VenueSelectionActivity.kt
@@ -1,15 +1,14 @@
 package com.mda.component_main.ui.activity
 
-import android.R.id.text1
 import android.graphics.*
 import android.os.Build
 import android.text.Layout
 import android.text.StaticLayout
 import android.text.TextPaint
-import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import com.alibaba.android.arouter.facade.annotation.Route
 import com.bin.david.form.core.SmartTable
 import com.bin.david.form.core.TableConfig
 import com.bin.david.form.data.CellInfo
@@ -27,10 +26,15 @@ import com.mda.common_ui_base.base.BaseVMDBActivity
 import com.mda.common_ui_base.base.BaseViewModel
 import com.mda.component_main.R
 import com.mda.component_main.databinding.ActivityVenueSelectionBinding
+import com.qmuiteam.qmui.widget.QMUITopBarLayout
+import com.qmuiteam.qmui.widget.tab.*
 
-
+@Route(path = "/cm/venueselectionactivity")
 class VenueSelectionActivity : BaseVMDBActivity<BaseViewModel, ActivityVenueSelectionBinding>() {
     private lateinit var table: SmartTable<Int>
+    private lateinit var mTopBar : QMUITopBarLayout
+    private lateinit var mTabSag : QMUITabSegment2
+
     override fun layoutId(): Int {
         return R.layout.activity_venue_selection
     }
@@ -42,13 +46,170 @@ class VenueSelectionActivity : BaseVMDBActivity<BaseViewModel, ActivityVenueSele
     override fun initView() {
         table = mDataBinding.tableSsss as SmartTable<Int>
 
+        mTopBar = mDataBinding.topbarVenueSelectionActivity
+
+        mTopBar.setTitle("场次选择")
+        mTopBar.addLeftBackImageButton().setImageDrawable(resources.getDrawable(R.drawable.icon_left_back))
+//            .setBackgroundColor(Color.parseColor("#333333"))
+//            .setImageDrawable(resources.getDrawable(R.drawable.icon_left_back))
+
+        mTabSag = mDataBinding.tabsagWeekVenueSelectionActivity
+        var tabBuilder : QMUITabBuilder =  mTabSag.tabBuilder()
+//        .build(this@VenueSelectionActivity)
+//        CustomeQMUITab("ssss\nssssss")
+        initTabSagment()
         inittable(table)
 
+    }
+
+    //初始化tagsagment
+    fun initTabSagment(){
+        var tabBuilder : QMUITabBuilder =  mTabSag.tabBuilder().setIconPosition(QMUITab.ICON_POSITION_TOP)
+        mTabSag.setMode(QMUITabSegment.MODE_SCROLLABLE)
+        mTabSag.setPadding(40, 0, 40, 0)
+        mTabSag.setIndicator(
+            QMUITabIndicator(
+                4, false, true
+            )
+        )
+        val component = tabBuilder
+            .setNormalDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.mipmap.icon_tabbar_component_selected1
+                )
+            )
+            .setSelectedDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.mipmap.testssss
+                )
+            )
+            .setText("1月11日")
+            .build(this@VenueSelectionActivity)
+
+        val component1 = tabBuilder
+            .setNormalDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.mipmap.icon_tabbar_component_selected
+                )
+            )
+            .setSelectedDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.drawable.week_test2
+                )
+            )
+            .setText("1月12日")
+            .build(this@VenueSelectionActivity)
+
+        val component2 = tabBuilder
+            .setNormalDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.mipmap.icon_tabbar_component_selected
+                )
+            )
+            .setSelectedDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.drawable.week_test2
+                )
+            )
+            .setText("1月13日")
+            .build(this@VenueSelectionActivity)
+
+        val component3 = tabBuilder
+            .setNormalDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.mipmap.icon_tabbar_component_selected
+                )
+            )
+            .setSelectedDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.drawable.week_test2
+                )
+            )
+            .setText("1月14日")
+            .build(this@VenueSelectionActivity)
+        val component4 = tabBuilder
+            .setNormalDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.mipmap.icon_tabbar_component_selected
+                )
+            )
+            .setSelectedDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.drawable.week_test2
+                )
+            )
+            .setText("1月15日")
+            .build(this@VenueSelectionActivity)
+
+        val component5 = tabBuilder
+            .setNormalDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.mipmap.icon_tabbar_component_selected
+                )
+            )
+            .setSelectedDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.drawable.week_test2
+                )
+            )
+            .setText("1月17日")
+            .build(this@VenueSelectionActivity)
+
+        val component6 = tabBuilder
+            .setNormalDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.mipmap.icon_tabbar_component_selected
+                )
+            )
+            .setSelectedDrawable(
+                ContextCompat.getDrawable(
+                    this@VenueSelectionActivity,
+                    R.drawable.week_test2
+                )
+            )
+            .setText("1月17日")
+            .build(this@VenueSelectionActivity)
+        mTabSag.addTab(
+            component
+        )
+        mTabSag.addTab(
+            component1
+        )
+        mTabSag.addTab(
+            component2
+        )
+        mTabSag.addTab(
+            component3
+        )
+        mTabSag.addTab(
+            component4
+        )
+        mTabSag.addTab(
+            component5
+        )
+        mTabSag.addTab(
+            component6
+        )
+        mTabSag.notifyDataChanged()
     }
 
     //初始化场地选择器
     fun inittable(table: SmartTable<Int>) {
         val week = arrayOf("日", "一", "二", "三", "四", "五", "六")
+        //按照列数据展示数据
         val infos = arrayOf(
             arrayOf(0, 1, 2, 1, 1, 0, 1, 1, 0, 1, 1, 2, 3),
             arrayOf(4, 2, 1, 1, 0, 1, 1, 0, 1, 1, 2, 2, 3),
@@ -120,7 +281,7 @@ class VenueSelectionActivity : BaseVMDBActivity<BaseViewModel, ActivityVenueSele
                     config: TableConfig
                 ): Int {
                     y = position
-                    return DensityUtils.dp2px(this@VenueSelectionActivity, 34f)
+                    return DensityUtils.dp2px(this@VenueSelectionActivity, 40f)
                 }
 
                 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -155,6 +316,7 @@ class VenueSelectionActivity : BaseVMDBActivity<BaseViewModel, ActivityVenueSele
 
                     }
 
+
                     //为不可预订时的处理
                     var bitmap = BitmapFactory.decodeResource(resources, R.drawable.icon_test1)
                     var zoomX = PhoneInfo.getPhonDensity(this@VenueSelectionActivity.application)
@@ -169,17 +331,18 @@ class VenueSelectionActivity : BaseVMDBActivity<BaseViewModel, ActivityVenueSele
                     //text
                     var paintText = Paint()
                     paintText.textSize = 13f * zoomX
+                    var w = paintText.measureText("¥50")
+
                     c.drawText(
                         "¥50",
-                        (rect.left + 5).toFloat(),
-                        (rect.top + 5).toFloat(),
+                        rect.centerX().toFloat() - w / 2,
+                        rect.centerY().toFloat() - (paintText.ascent() + paintText.descent()) / 2,
                         paintText
                     )
-                    var w = paintText.measureText("¥50")
 
                     var shader: Shader =
                         BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
-//                    paintBitmap.setShader(shader)
+
                     c.drawRoundRect(
                         (rect.left + 5).toFloat(),
                         (rect.top + 5).toFloat(),
@@ -189,8 +352,8 @@ class VenueSelectionActivity : BaseVMDBActivity<BaseViewModel, ActivityVenueSele
                         paintBackground
                     )
 
-                    var widthM = (60f * zoomX - bitmap.width) / 2
-                    var heightM = (34f * zoomX - bitmap.height) / 2
+                    var widthM = (80f * zoomX - bitmap.width) / 2
+                    var heightM = (40f * zoomX - bitmap.height) / 2
 
 
                     c.drawBitmap(
@@ -219,13 +382,15 @@ class VenueSelectionActivity : BaseVMDBActivity<BaseViewModel, ActivityVenueSele
             override fun format(t: Int?): String {
                 when (t) {
 
-                    1 -> return "场地"
+                    0 -> return "场地"
 
                     else -> {
 
 
                         //TODO 返回时间列表
-                        var s = StringBuilder().append("09:00").append("\n").append("-11:00")
+                        var s =
+                            StringBuilder().append("09:00").append("\n").append("-").append("\n")
+                                .append("11:00")
                         return s.toString()
                     }
 
@@ -233,10 +398,9 @@ class VenueSelectionActivity : BaseVMDBActivity<BaseViewModel, ActivityVenueSele
             }
 
 
-
             override fun draw(canvas: Canvas?, sequence: Int, rect: Rect?, config: TableConfig?) {
                 var text1 = "Lorem\nindustry."
-                var t = format((sequence + 1))
+                var t = format((sequence))
 
 
 //                val staticLayout2 = StaticLayout.Builder.obtain()
@@ -249,21 +413,32 @@ class VenueSelectionActivity : BaseVMDBActivity<BaseViewModel, ActivityVenueSele
 
 
                 var paint1 = TextPaint()
-                paint1.setColor(Color.parseColor("#3399fe"))
+                paint1.isAntiAlias = true
+                paint1.textAlign = Paint.Align.CENTER
+                paint1.textSize = 25 * zoom
+                paint1.setColor(Color.parseColor("#333333"))
+//                paint1.setColor(Color.parseColor("#3399fe"))
                 val staticLayout1 = StaticLayout(
-                    text1, paint1, 80,
+                    t, paint1, 100,
                     Layout.Alignment.ALIGN_NORMAL, 1f, 0f, true
                 )
 //                staticLayout1.toString()
-                canvas!!.save()
-                canvas.translate(50f, 100f)
-                staticLayout1.draw(canvas!!)
-                canvas!!.restore()
-//                canvas!!.drawText(
-//                    format((sequence + 1)), rect!!.centerX().toFloat(), DrawUtils.getTextCenterY(
-//                        rect.centerY(), paint
-//                    ), paint
-//                )
+                if (sequence != 0 && sequence < 14) {
+
+                    canvas!!.save()
+                    canvas.translate(
+                        rect!!.centerX().toFloat(),
+                        (rect.centerY() - staticLayout1.height / 2).toFloat()
+                    )
+                    staticLayout1.draw(canvas!!)
+                    canvas!!.restore()
+                } else {
+                    canvas!!.drawText(
+                        format((sequence)), rect!!.centerX().toFloat(), DrawUtils.getTextCenterY(
+                            rect.centerY(), paint
+                        ), paint
+                    )
+                }
 
 
             }
