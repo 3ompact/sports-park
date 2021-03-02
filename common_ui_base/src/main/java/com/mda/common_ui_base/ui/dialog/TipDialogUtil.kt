17be3c2b5bdd7aaefbdd2f1b600e521343ba0814
3ompact <qhwwjf@gmail.com>
2021-03-02 15:40:28 +0800

    add TipDialogUtil.kt of loadingdialog

--- common_ui_base/src/main/java/com/mda/common_ui_base/ui/dialog/TipDialogUtil.kt
+++ common_ui_base/src/main/java/com/mda/common_ui_base/ui/dialog/TipDialogUtil.kt
@@ -1,6 +1,7 @@
 package com.mda.common_ui_base.ui.dialog
 
 import android.content.Context
+import com.mda.common_ui_base.R
 import com.qmuiteam.qmui.widget.dialog.QMUITipDialog
 
 /**
@@ -11,19 +12,45 @@ class TipDialogUtil {
     /**
      * 获取一般实例
      */
-    fun getNormalInstance(context:Context,title:String):QMUITipDialog{
+    fun getNormalInstance(context: Context, title: String): QMUITipDialog {
         return QMUITipDialog.Builder(context).setTipWord(title).create()
     }
+
     /**
      * 获取带icon实例
      */
-    fun getWithIconInstance(context:Context,title:String,iconType:Int):QMUITipDialog{
+    fun getWithIconInstance(context: Context, title: String, iconType: Int): QMUITipDialog {
         return QMUITipDialog.Builder(context).setIconType(iconType).setTipWord(title).create()
     }
+
     /**
      * 获取自定义布局实例
      */
-    fun getWithCustomInstance(context:Context,layoutRes:Int):QMUITipDialog{
+    fun getWithCustomInstance(context: Context, layoutRes: Int): QMUITipDialog {
         return QMUITipDialog.CustomBuilder(context).setContent(layoutRes).create()
+    }
+
+    /**
+     * 加载中
+     * dialog
+     */
+    fun showLodingDialog(context: Context) {
+        val tipDialog = QMUITipDialog.Builder(context)
+            .setIconType(QMUITipDialog.Builder.ICON_TYPE_LOADING)
+            .setTipWord("正在加载")
+            .create()
+            .show()
+    }
+
+    /**
+     * 加载中
+     * dialog
+     * 获取示例
+     */
+    fun getLodingDialog(context: Context): QMUITipDialog {
+        return QMUITipDialog.Builder(context)
+            .setIconType(QMUITipDialog.Builder.ICON_TYPE_LOADING)
+            .setTipWord("正在加载")
+            .create()
     }
 }
